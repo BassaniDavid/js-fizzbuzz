b68a0ab9821361004e209de9519eeedd0affa82f
BassaniDavid <davbassan98@gmail.com>
2025-02-25 14:16:37 +0100

    feat: print the number only if the precedent conditions are false

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
-for (let i = 1; i <= 30; i++) {
-    console.log(i)
+for (let i = 1; i <= 100; i++) {
+    
     if(i % 3 == 0 && i % 5 !== 0 ) {
         console.log("Fizz")
     }
-    if(i % 5 == 0 && i % 3 !== 0) {
+    else if(i % 5 == 0 && i % 3 !== 0) {
         console.log("Buzz")
     }
-    if(i % 5 == 0 && i % 3 == 0) {
+    else if(i % 5 == 0 && i % 3 == 0) {
         console.log("FizzBuzz")
     }
+    else console.log(i)
 
 }
