e032d04ab5b42b1408f0b249b267ec9c393dfb6a
BassaniDavid <davbassan98@gmail.com>
2025-02-25 14:13:58 +0100

    feat: is the number is divisible for 3 and 5 don't print Bizz neither Buzz

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 30; i++) {
     console.log(i)
-    if(i % 3 == 0 ) {
+    if(i % 3 == 0 && i % 5 !== 0 ) {
         console.log("Bizz")
     }
-    if(i % 5 == 0 ) {
+    if(i % 5 == 0 && i % 3 !== 0) {
         console.log("Buzz")
     }
 }
